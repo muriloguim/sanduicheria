f66535e8dad4f5866a3cfd0aa69d5164661a620d
Murilo Guimaraes <murilogsilva@gmail.com>
2024-05-31 23:48:22 -0300

    Initial commit

---
+++ src/main/java/com/sanduicheria/burguer/management/infrastruture/entity/ProductEntity.java
@@ -0,0 +1,23 @@
+package com.sanduicheria.burguer.management.infrastruture.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.*;
+
+@Document(collection = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductEntity {
+
+    @Id
+    private String id;
+    private String name;
+    private String price;
+    private String description;
+    private String category;
+
+}
